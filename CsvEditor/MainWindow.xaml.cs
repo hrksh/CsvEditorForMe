c0e36d99f71075653c7fc2ce9f5247fde0b3234e
r hrksh <r_hirakoshi@outlook.com>
2024-10-09 22:49:16 +0900

    プロジェクトファイル一式追加とサンプル実装

---
+++ CsvEditor/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CsvEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// テスト用クリックイベント
+        /// </summary>
+        /// <param name="sender">イベント元</param>
+        /// <param name="e">イベント引数</param>
+        private void btnTest_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("hello world.");
+        }
+    }
+}
