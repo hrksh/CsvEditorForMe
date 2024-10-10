ca4c3cffa2d9b102b871aa04d40b9277d052cebe
r hrksh <r_hirakoshi@outlook.com>
2024-10-10 23:53:50 +0900

    レガシーな感じのメニューバーを追加

--- CsvEditor/MainWindow.xaml.cs
+++ CsvEditor/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.ComponentModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -22,6 +23,11 @@ namespace CsvEditor
         public MainWindow()
         {
             InitializeComponent();
+
+            if ((bool)DesignerProperties.GetIsInDesignMode(this))
+            {
+
+            }
         }
         /// <summary>
         /// テスト用クリックイベント
